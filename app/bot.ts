99ff6d54ef98cc504529b46f598316cec57686df
jrCleber <cleber.jr@live.com>
2021-12-30 08:56:28 -0300

    bot.ts

--- app/bot.ts
+++ app/bot.ts
@@ -3,13 +3,13 @@ import { create, Whatsapp } from 'venom-bot'
 // importando o controller da aplicação
 import { AppController } from "./controller/appController"
 // importando as configurações do bot
-import botConfig from './settings/settingsBot.json'
+import botConfig from './jsonConfig/settingsBot.json'
 //importando nome das collections
 import collection from './data/collectionsNames.json'
 // importando opções de respostas/comando
-import myCommands from './chatManage/commands'
+import { initCommands } from './chatManage/commands'
 // importando o gerenciamento de chat
-import manageChat from './chatManage/managementChat'
+import { manageChat } from './chatManage/managementChat'
 
 // tipos da mensagens que receberão tratamentos diferentes
 const arrayTypes = ['image', 'location', 'broadcast', 'ptt', 'video', 'sticker', 'document', 'vcard', 'audio']
@@ -40,17 +40,27 @@ export function bot() {
             // verificando se o tipo da mensagem não está incluso nop arraytypes
             if (arrayTypes.includes(message.type) === false && message.isGroupMsg === false && message.hasOwnProperty('body')) {
                 // recuperando estágios do cliente
-                // se o retorno for nulo, é um cliente novo
+                // se o retornar um valor false é um cliente novo
                 const chatState = await chatControll.getDocumetId(message.chatId)
                 if (chatState?.exists) {
-                    command = 'initChat'
-                } else {
                     // se sim: passar as configurações de estágio do checkState
-                    command = chatState?.codeState
+                    command = chatState?.data().codeState
+                } else {
+                    // se não: passar o comando de início de chat
+                    command = 'initChat'
                 }
-                // setando o comando para gerenciar o chat
-                for (const [key, value] of Object.entries(myCommands)) {
-                    if (value(message) === true) {
+                /* 
+                    fazendo a varredura para verificar se o comando existe na lista de comandos
+                    caso os estágios do cliente estejam zerados no banco.
+                    isso acontece, pois no final do atendimento, os estágios do cliente serão reiniciados no banco,
+                    ou quando o cliente estiver em algum subestágio do atendimento
+                */
+                for (const [key, func] of Object.entries(initCommands)) {
+                    /*
+                        verificando se o cliente enviou algum comando válido e atribuindo o comando na variável command
+                        se não, mantém-se o valor da variável command do bloco if
+                    */
+                    if (func(message) === true) {
                         command = key as keyof typeof manageChat
                         break
                     }
@@ -61,6 +71,8 @@ export function bot() {
                 // verificando se a referência da função é verdadeira
                 if (chatManagement !== undefined) {
                     chatManagement(message, client)
+                } else {
+                    // enviar para o cliente uma mensagem dizendo que não foi possível compreender a sua intenção
                 }
             }
         })
