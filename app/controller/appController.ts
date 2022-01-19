869c0f80281c5f291320fd5deed5e50ad11fe8c5
cleberWilson <cleber.jr@live.com>
2022-01-19 12:42:28 -0300

    comentários adicionados

--- app/controller/appController.ts
+++ app/controller/appController.ts
@@ -1,7 +1,17 @@
 import { DbConn } from '../../connections/dbConnection'
 import { firestore } from 'firebase-admin'
 
-// criando o controller que fará o CRUD
+/**
+ * criando o controller que fará o CRUD
+ * 
+ * @AddData https://firebase.google.com/docs/firestore/manage-data/add-data#set_a_document
+ * @UpdateData https://firebase.google.com/docs/firestore/manage-data/add-data#update-data
+ * @GetData https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document
+ * @GedAllData https://firebase.google.com/docs/firestore/query-data/get-data#get_multiple_documents_from_a_collection
+ * @SimpleQuery https://firebase.google.com/docs/firestore/query-data/queries#simple_queries
+ * @QueryOperators https://firebase.google.com/docs/firestore/query-data/queries#query_operators
+ * @CompoundQueries https://firebase.google.com/docs/firestore/query-data/queries#compound_queries
+*/
 export class AppController {
     constructor(
         public collection: string
@@ -10,7 +20,11 @@ export class AppController {
     // passando o nome da collection 
     private readonly _db = new DbConn(this.collection).instance.firestore()
 
-    // retornando todos os documentos da collection
+    /**
+     * retornando todos os documentos da collection
+     * 
+     * @returns retorna uma lista dom todos os documetos da collection
+     */
     async getAllDocuments(): Promise<firestore.QueryDocumentSnapshot<firestore.DocumentData>[] | null> {
         try {
             const querySnapshots = await this._db.collection(this.collection).get()
@@ -21,8 +35,13 @@ export class AppController {
         }
     }
 
-    // retornado documento pelo id
-    async getDocumetId(idDoc: string): Promise<firestore.DocumentData | null | undefined> {
+    /**
+     * retornado documento pelo id
+     * 
+     * @param idDoc uma string contendo o id do documento a ser retornado
+     * @returns retorna o documento encontrado ou null
+     */
+    async getDocumetId(idDoc: string): Promise<firestore.DocumentData | null> {
         try {
             const documentReference = await this._db.collection(this.collection).doc(idDoc).get()
             return documentReference
@@ -32,8 +51,16 @@ export class AppController {
         }
     }
 
-    // recuperando documento pelo id com a cláusula where
-    async getDocumentIdWhere(
+    /**
+     * recuperando documento pelo id com a cláusula where
+     * 
+     * @param field nome do campo para realizar a comparação
+     * @param opStr operadores de comparação === | == | >= | <= | !== | != | array-contains | array-contains-any | in | not-in
+     * @Docs https://firebase.google.com/docs/firestore/query-data/queries
+     * @param value valor a ser comparado podem ser dos tipos string | boolen | number | null | array | map
+     * @returns retorna uma lista com os documentos encontrados
+     */
+    async getDocumentWhere(
         field: string | firestore.FieldPath,
         opStr: firestore.WhereFilterOp,
         value: any
@@ -49,7 +76,13 @@ export class AppController {
         }
     }
 
-    // inserção com id dinâmico
+    /**
+     * inserção com id dinâmico
+     * 
+     * @param data um objeto do tipo { key: value } - onde key: string e value: string | boolen | number | null | array | map
+     * mas lembre-se: nem um desses valores pode ser do tipo undefined ou conter um undefined. O tipo undefined não é suportado pelo firebase
+     * @returns retorna o id gerado dinamicamente pelo banco
+     */
     async insertDoc(data: any): Promise<string> {
         try {
             // adicionando registro temporal no cadastro
@@ -63,8 +96,16 @@ export class AppController {
         }
     }
 
-    // inserção com id definido
-    insertDocWithId(idDoc: string, data: any, insertDate = true) {
+    /**
+     * inserção com id definido
+     * 
+     * @param idDoc uma string que referencia o documento a ser deletado
+     * @param data um objeto do tipo { key: value } - onde key: string e value: string | boolen | number | null | array | map
+     * mas lembre-se: nem um desses valores pode ser do tipo undefined ou conter um undefined. O tipo undefined não é suportado pelo firebase
+     * @param insertDate um valor booleano que determina a incerção de um timestamp no documento
+     * @default insertDate = true
+     */
+    insertDocWithId(idDoc: string, data: any, insertDate: boolean = true): void {
         try {
             // adicionando registro temporal no cadastro
             if (insertDate) {
@@ -77,8 +118,18 @@ export class AppController {
         }
     }
 
-    // atualizar um documento
-    async updateDoc(idDoc: string, field: string, fieldValue: any, insertDate = true): Promise<firestore.WriteResult | undefined> {
+    /**
+     * atualizar um documento
+     * 
+     * @param idDoc uma string que referencia o documento a ser deletado
+     * @param field nome do campo a ser atualizado no documento
+     * @param fieldValue um valor que deve ser do tipo string | boolen | number | null | array | map
+     * mas lembre-se: nem um desses valores pode ser do tipo undefined ou conter um undefined. O tipo undefined não é suportado pelo firebase
+     * @param insertDate um valor booleano que determina a incerção de um timestamp no documento
+     * @default {boolean} insertDate = true
+     * @returns retorna um documento com o resultado da atualização
+     */
+    async updateDoc(idDoc: string, field: string, fieldValue: any, insertDate: boolean = true): Promise<firestore.WriteResult | undefined> {
         try {
             let data = { [field]: fieldValue }
             // adicionando registro temporal no cadastro
@@ -92,18 +143,22 @@ export class AppController {
         }
     }
 
-    // atualizando vários campos de um vez
-    /*  
-        esta função não isere no banco o date time da atualização automaticamente.
-        você insere nos campos que quer atualizar por ex:
-
+    /**
+     * atualizando vários campos de um vez
+     * 
+     * esta função não isere no banco o date time da atualização automaticamente.
+     * você insere nos campos que quer atualizar por ex:
         const manyFields = {
             name: 'seu nome',
             sobreNome: 'seu sobre nome',
             dataAtualizacao: Date.now() || new Date() || firestore.FieldValue.serverTimestamp()
         }
-        updateManyFields('seu id', manyFields)   
-    */
+        updateManyFields('seu id', manyFields)  
+    
+     * @param idDoc uma que string referencia o documento a ser atualizado
+     * @param manyFields um objeto do tipo { key: value } como parâmetros para a atualização
+     * @returns} retorna um documento com o resultado da atualização
+     */
     async updateManyFields(idDoc: string, manyFields: any): Promise<firestore.WriteResult | undefined> {
         try {
             const writeResult = await this._db.collection(this.collection).doc(idDoc).update(manyFields)
@@ -113,7 +168,12 @@ export class AppController {
         }
     }
 
-    // deletendo documento
+    /**
+     * deletando um documento
+     * 
+     * @param idDoc uma string que referencia o documento a ser deletado
+     * @returns retorna um documento com o resultado da deleção
+     */
     async deleteDoc(idDoc: string): Promise<firestore.WriteResult | undefined> {
         try {
             const res = await this._db.collection(this.collection).doc(idDoc).delete()
