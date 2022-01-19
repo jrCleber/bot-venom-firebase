ffa5848c73fad0fa446404e1c4787570bb19f323
cleberWilson <cleber.jr@live.com>
2022-01-19 12:53:16 -0300

    comentários atualizados

--- connections/dbConnection.ts
+++ connections/dbConnection.ts
@@ -1,7 +1,10 @@
 import * as admin from 'firebase-admin'
 import key from '../keys/serviceAccountKey.json'
 
-// configurando a connexão com o banco de dados
+/**
+ * configurando a connexão com o banco de dados
+ * @AccountKey https://console.firebase.google.com/u/0/project/webbot-dbc99/settings/serviceaccounts/adminsdk
+ */
 export class DbConn {
     constructor(
         public nameApp?: string,
@@ -10,12 +13,14 @@ export class DbConn {
             projectId: key.project_id,
             clientEmail: key.client_email
         },
-        // a função initializeApp recebe dois parâmetros
-        // 1 - objeto para a inicialização do DbConn
-        // 2 - "name": o name é utilizado para diferenciar as instâncias e evitar conflitos
+        /**
+         * a função initializeApp recebe dois parâmetros
+         * 1 - objeto para a inicialização do DbConn
+         * 2 (opcional) - "name": o name é utilizado para diferenciar as instâncias e evitar conflitos
+         */
         public readonly instance = admin.initializeApp(
             { credential: admin.credential.cert(_serviceAccountKey) },
             nameApp?.split('').sort(() =>  0.5-Math.random()).join('')
-        )
+        ).firestore()
     ) { }
 }
